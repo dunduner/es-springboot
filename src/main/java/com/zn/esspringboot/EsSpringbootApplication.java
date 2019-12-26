56d6befaa0cea1ddc23dda7340445fac7e38ab6a
HUAWEI <253013263@qq.com>
2019-12-26 17:03:53 +0800

    es-springboot

---
+++ src/main/java/com/zn/esspringboot/EsSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.zn.esspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsSpringbootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EsSpringbootApplication.class, args);
+    }
+
+}
